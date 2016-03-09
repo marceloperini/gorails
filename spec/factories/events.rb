79df14db86b1f2cb73b98b05cf4e5d87a5d4eb58
HenriqueGeek <henrique@dryworks.com.br>
2016-03-08 21:24:47 -0300

    Edita factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :event do
   	name 'MyEvent'
   	description 'MyDescription'
-  	date '2016-01-01'
+  	start_at '2016-01-01'
+  	end_at '2016-01-01'
   	local 'MyLocal'
   	participants_limit 10
   end
