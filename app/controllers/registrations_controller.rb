00bde7249ffe768fcc319e315f185feba35a45b4
João Carlos Ottobboni <jcottobboni@gmail.com>
2016-03-09 14:15:08 -0300

    Update registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,9 @@
+# app/controllers/registrations_controller.rb
 class RegistrationsController < Devise::RegistrationsController
-
   private
-
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :nickname,:email, :password, :password_confirmation, :current_password)
   end
