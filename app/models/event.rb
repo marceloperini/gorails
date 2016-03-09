b4173777283cb6cfef77837821e1b8b5dc87932e
João Carlos Ottobboni <jcottobboni@gmail.com>
2016-03-09 14:17:39 -0300

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
+# app/models/event.rb
 class Event < ActiveRecord::Base
 
 	belongs_to :user
@@ -8,14 +9,14 @@ class Event < ActiveRecord::Base
 
   alias_method :is_registrated?, :registrations_is_registrated?
   alias_method :to_register, :registrations_to_register
-
+# Returns false if event is full
   def exceeded_limit?
   	return true if self.registrations.size >= self.participants_limit
   	false
   end
 
   validates_presence_of :name,:description,:local,:participants_limit,:start_at,:end_at
-
+# Returns duration of event
   def event_duration
     (( self.end_at  - self.start_at) / 1.hour).round
   end
