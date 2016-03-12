1c1b7c99e773fa9c48958d4f7594657e590b5cac
HenriqueGeek <henrique@dryworks.com.br>
2016-03-12 05:26:44 -0300

    Cria metodo all para ser usado com qualquer provider

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,15 +2,19 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   skip_before_filter :authenticate_user!
 
-  def google_oauth2
+  def all
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.user_attributes"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
+
+  alias_method :facebook, :all
+  alias_method :passthru, :all
+  alias_method :google_oauth2, :all
 end
