02038893d69ff061276f19b955d52aacf468bc6d
helioborges <helio.borgesjr@gmail.com>
2016-03-12 10:49:21 -0300

    Teste

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -35,7 +35,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to @event, notice: 'Evento foi criado com sucesso!' }
         format.json { render action: 'show', status: :created, location: @event }
       else
         format.html { render action: 'new' }
