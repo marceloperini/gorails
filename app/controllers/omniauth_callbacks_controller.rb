9716084a7fa85366f812812bfd3446522f4fe9ed
HenriqueGeek <henrique@dryworks.com.br>
2016-03-13 14:14:15 -0300

    Refatora metodo para aceitar linkedin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -17,6 +17,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   alias_method :google_oauth2, :all
   alias_method :facebook, :all
   alias_method :github, :all
+  alias_method :linkedin, :all
   alias_method :passthru, :all
 
 end
