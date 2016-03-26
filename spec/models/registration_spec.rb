b19ede43dd1990f00831f9777528489b47523f55
Henrique H. da Silva <henrique@dryworks.com.br>
2016-03-26 18:22:03 -0300

    Edita specs

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Registration, type: :model do
 
-	let(:user) { create :user }
-	let(:event) { create :event }
+  let(:user) { create :user_with_cpf }
+  let(:event) { create :event }
 
-	describe ".is_registrated?" do
-		subject(:registrated) { event.is_registrated?(user.id) }
+  describe ".is_registrated?" do
+    subject(:registrated) { event.is_registrated?(user.id) }
 
-		context 'when user is registrate' do
-			before(:each) do
-			  create :registration, user: user, event: event
-			end
+    context 'when user is registrate' do
+      before(:each) do
+        create :registration, user: user, event: event
+      end
 
-			it 'return true' do
-				expect(registrated).to be_truthy
-			end
-		end
+      it 'return true' do
+        expect(registrated).to be_truthy
+      end
+    end
 
-		context 'when user is not registrate' do
-			it 'return false' do
-				expect(registrated).to be_falsey
-			end
-		end
-	end
+    context 'when user is not registrate' do
+      it 'return false' do
+        expect(registrated).to be_falsey
+      end
+    end
+  end
 
-	describe '.to_register' do
-	  it 'registration should be created' do
-	  	event.to_register(user.id)
-	  	expect(Registration.all.size).to eq 1
-	  end
-	end
+  describe '.to_register' do
+    it 'registration should be created' do
+      event.to_register(user.id)
+      expect(Registration.all.size).to eq 1
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	describe "#has_valid_cpf" do
-		context 'when cpf valid' do
-			let(:user_valid) { create :user}
+  describe "#has_valid_cpf" do
+    context 'when cpf valid' do
+      let(:user_valid) { create :user_with_cpf}
 
-			it 'return true' do
-				expect(user_valid.has_valid_cpf?).to be_truthy
-			end
-		end
-	end
+      it 'return true' do
+        expect(user_valid.has_valid_cpf?).to be_truthy
+      end
+    end
+  end
 end
