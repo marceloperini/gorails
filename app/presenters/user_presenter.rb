8bf8e58fab2e75bc956316ec0ed97847766d0b95
jcottobboni <jcottobboni@gmail.com>
2016-03-27 16:12:32 -0300

    Fixed #58

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,10 +1,10 @@
 class UserPresenter < Burgundy::Item
 
-  def user_avatar(css=true,size="")
+  def user_avatar(css=true,size="",tooltip=false)
     if item.avatar.present?
-      h.image_tag(item.avatar.url, class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : ""  )
+      h.image_tag(item.avatar.url, class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : ""  ,"data-toggle"=>"#{tooltip ? 'tooltip':''}", "data-placement"=>"#{tooltip ? 'top':''}",  'data-original-title'=>"#{tooltip ? "#{item.event_name}" :''}" )
     else
-      h.image_tag(gravatar_url(item), class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : "" )
+      h.image_tag(gravatar_url(item), class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : "" ,"data-toggle"=>"#{tooltip ? 'tooltip':''}", "data-placement"=>"#{tooltip ? 'top':''}",  'data-original-title'=>"#{tooltip ? "#{item.event_name}" :''}")
     end
   end
 
@@ -14,5 +14,7 @@ class UserPresenter < Burgundy::Item
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{default_url}"
   end
+
+
 
 end
