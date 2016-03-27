9965aed49a0c1c44c980968d74f473fcd33cd8d4
jcottobboni <jcottobboni@gmail.com>
2016-03-27 15:53:08 -0300

    Fixed #58

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -4,15 +4,15 @@ class UserPresenter < Burgundy::Item
     if item.avatar.present?
       h.image_tag(item.avatar.url, class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : ""  )
     else
-      h.image_tag(avatar_url(item), class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : "" )
+      h.image_tag(gravatar_url(item), class: css ? "img-avatar img-avatar#{size} img-avatar-thumb" : "" )
     end
   end
 
 
-  def avatar_url(user)
-      default_url = "user.png"
+  def gravatar_url(user)
+      default_url = "http://www.gorails.com.br/#{ h.asset_path 'user.png'}"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{default_url}"
   end
 
 end
