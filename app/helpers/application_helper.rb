588731abc353edfcfa7629c97b0719a0ca3367e4
jcottobboni <jcottobboni@gmail.com>
2016-04-04 13:22:00 -0300

    add partners static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,6 +54,9 @@ module ApplicationHelper
     end
   end
 
+  def link_to_image(image_path, target_link,options={})
+    link_to(image_tag(image_path, :border => "0",class: "image-size"), target_link, options)
+  end
 
 
 end
