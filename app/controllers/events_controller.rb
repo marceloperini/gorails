9773f211ac533fb31957ddef49e2661f44953d53
Henrique H. da Silva <henrique@dryworks.com.br>
2016-04-23 18:40:30 -0300

    Edita events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -95,8 +95,8 @@ class EventsController < ApplicationController
 
   def register_user
     @event.to_register(set_user.id)
-    params.merge!(register: {}.merge!(need_certificate: "0")) if params[:register].nil?
-    update_user_need_certificate if params[:register][:need_certificate]=="1"
+    params[:register] = {}.merge!(need_certificate: "0") if params[:register].nil?
+    update_user_need_certificate if params[:register][:need_certificate] == "1"
     register_success
   end
 
