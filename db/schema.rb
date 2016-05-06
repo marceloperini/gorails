b0d85378d2c792ace5acde02505f0a2355eb48ab
jcottobboni <jcottobboni@gmail.com>
2016-05-06 20:17:41 -0300

    Add required files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414002307) do
+ActiveRecord::Schema.define(version: 20160506005628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,18 +178,18 @@ ActiveRecord::Schema.define(version: 20160414002307) do
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                   default: "",   null: false
+    t.string   "encrypted_password",      default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",           default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "cpf"
@@ -222,6 +222,9 @@ ActiveRecord::Schema.define(version: 20160414002307) do
     t.boolean  "need_certificate"
     t.boolean  "digital_certificate"
     t.boolean  "printed_certificate"
+    t.boolean  "receber_email",           default: true
+    t.boolean  "receber_email_parceiros", default: true
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
