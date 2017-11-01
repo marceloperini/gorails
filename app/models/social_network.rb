19c2e3629bd8da972df84d10aa8beb1f2078075c
Benhur <benhur10cp@gmail.com>
2017-10-31 23:20:19 -0200

    fixed #183

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -1,4 +1,27 @@
 class SocialNetwork < ActiveRecord::Base
   belongs_to :social_network_type
   belongs_to :user
+
+  validates_presence_of :link, :social_network_type_id
+
+
+  HUMANIZED_ATTRIBUTES = {
+      id: "Rede Social",
+      link: "Link da Rede Social",
+      social_network_type_id: "Tipo Rede Social"
+  }
+
+  def self.human_attribute_name(attr, vazio=nil)
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      lista = []
+      column_names.each {|coluna| lista << self.human_attribute_name(coluna)}
+      csv << lista
+      all.each {|registro| csv << registro.attributes.values_at(*column_names)}
+    end
+  end
+
 end
