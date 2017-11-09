2c7322e27eac30013e0e991c75b465e4217a401e
HELIO BORGES <helio.borgesjr@gmail.com>
2017-11-09 21:24:27 -0200

    resolvendo os choros do houndci-bot

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@ class HomeController < ApplicationController
     @news = Blog::Post.tagged_with('noticias').where('? >= published_at and draft = ?', DateTime.now, false)
                 .includes(:user).limit(5).order(published_at: :desc)
 
-    @jobs = Jobs::Job.all.where(status: true).order(expiration_date: :desc).limit(5)
+    @jobs=Jobs::Job.where(status: true).order(expiration_date: :desc).limit(5)
 
-    @events = Event.not_happened_yet(DateTime.now).order(start_at: :asc).limit(5)
+    @events=Event.not_happened_yet(Time.now).order(start_at: :asc).limit(5)
 
-@transactions = FinancialTransaction.all.order(payment_date: :asc).limit(5)
+    @transactions=FinancialTransaction.all.order(payment_date: :asc).limit(5)
   end
 end
