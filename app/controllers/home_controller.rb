130549f35368bde25ac1fa6f1fea9d0897f7cb72
HELIO BORGES <helio.borgesjr@gmail.com>
2017-11-09 21:19:27 -0200

    tarefa #351

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def index
     @news = Blog::Post.tagged_with('noticias').where('? >= published_at and draft = ?', DateTime.now, false)
                 .includes(:user).limit(5).order(published_at: :desc)
+
+    @jobs = Jobs::Job.all.where(status: true).order(expiration_date: :desc).limit(5)
+
+    @events = Event.not_happened_yet(DateTime.now).order(start_at: :asc).limit(5)
+
+@transactions = FinancialTransaction.all.order(payment_date: :asc).limit(5)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -24,6 +24,8 @@ class Event < ActiveRecord::Base
   alias_method :is_registrated?, :registrations_is_registrated?
   alias_method :to_register, :registrations_to_register
 
+  scope :not_happened_yet, -> (limit_date) { where("end_at > ?", limit_date )}
+
 # Returns false if event is full
   def exceeded_limit?
     return true if self.registrations.size >= self.participants_limit
